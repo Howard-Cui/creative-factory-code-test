6d49aa7a9e23e2aab528e685ea31d6955a701154
Howard-Cui <howard@itrazotracetech.com>
2024-10-05 10:50:07 +1000

    fix: use cdn image from country-code-flag for avoid incompatible issue in windows with react-country-flag

--- src/components/CurrencyInfoContainer/index.tsx
+++ src/components/CurrencyInfoContainer/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactCountryFlag from 'react-country-flag';
 
 interface CurrencyInfoContainerProps {
 	rightSlot?: React.ReactNode;
@@ -18,12 +17,18 @@ const CurrencyInfoContainer = ({
 }: CurrencyInfoContainerProps) => {
 	return (
 		<div
-			className={`w-4/5 h-14 rounded-md border-normal_grey hover:border-focus box-content border
+			className={`w-4/5 max-w-[500px] h-14 rounded-md border-normal_grey hover:border-focus box-content border
 		 border-2 flex justify-between box-content ${className}`}
 		>
 			<div className="w-11/12 flex justify-between items-center px-2">
 				<div className="flex gap-3 items-center h-full">
-					<ReactCountryFlag countryCode={countryCode} />
+					<div
+						// use inline style here for insert image in more flexible way, because tailwind not support string concat for classes
+						style={{
+							backgroundImage: `url(http://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode}.svg)`,
+						}}
+						className="flex justify-end w-4 h-4 rounded-full bg-no-repeat bg-center bg-cover"
+					></div>
 					<h3>{currencyCodeDisplay}</h3>
 				</div>
 				{children}
